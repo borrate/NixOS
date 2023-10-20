7d51fc70ccf8fd784c47d6939ae4a4628e0d30e8
Rodrigo <rodrigo@rhm.cl>
2023-10-20 20:41:58 -0300

    feat: Agrega Dejavu y firacode nerd

--- configuration.nix
+++ configuration.nix
@@ -129,6 +129,12 @@
       ];
   };
 
+  fonts.fonts = with pkgs; [
+      dejavu_fonts
+      font-awesome
+      (nerdfonts.override { fonts = [ "FiraCode" "DejaVuSansMono" ]; })
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
