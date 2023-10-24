f200311795a484256fb649a4797ee0e88118a2ed
Rodrigo <rodrigo@rhm.cl>
2023-10-24 14:44:44 -0300

    feat: Agrega configuración de starship

--- configuration.nix
+++ configuration.nix
@@ -72,7 +72,6 @@
     nodejs
     obsidian
     ripgrep
-    starship
     tig
     tldr
     traceroute
@@ -89,6 +88,39 @@
     enable = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    # "$schema" = "https://starship.rs/config-schema.json";
+    settings = {
+      right_format = "$time $sudo";
+      format = ''
+      $directory$git_branch$git_status
+      $character
+      '';
+      directory = {
+        truncation_length = 2;
+        truncation_symbol = "…/";
+      };
+      git_branch = {
+        format = "[\[$symbol$branch(:$remote_branch)\]]($style)";
+        style = "bold purple";
+      };
+      sudo = {
+        disabled = false;
+        format = "[$symbol]($style)";
+      };
+      time = {
+        disabled = false;
+        format = "[$time]($style)";
+      };
+      env_var.USER = {
+        default = "nn";
+        format = "[$env_value]($style)";
+        style = "201 bold"; # Ansi color 201 https://i.stack.imgur.com/KTSQa.png
+      };
+    };
   };
 
   programs.tmux = {
