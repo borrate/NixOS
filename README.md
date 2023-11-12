0dc43905a742c088c5370a4d79e9381efde80120
Rodrigo <rodrigo@rhm.cl>
2023-11-12 18:46:03 -0300

    !feat: Agrega Home Manager
    - Mueve wezterm desde configuration.nix
    - Mueve starship desde configuration.nix
    - Actualiza README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # NixOS
 Configuración NixOS
 
-Este repositorio lo tengo clonado en mi usuario y luego symlink hacia `/etc/nixos/`
-El archivo `hardware-configuration.nix` debe ser copiado deste `/etc/nixos/` y luego symlinkeado de vuelta.
+- Este repositorio lo tengo clonado en mi usuario y luego symlink hacia `/etc/nixos/`
+- El archivo `hardware-configuration.nix` debe ser copiado deste `/etc/nixos/` y luego symlinkeado de vuelta.
+- La carpeta `home-manager` está symlinkeada dentro de `~/.config/`
---
+++ home-manager/apps/starship.nix
@@ -0,0 +1,34 @@
+{
+  programs.starship = {
+    enable = true;
+    # "$schema" = "https://starship.rs/config-schema.json";
+    settings = {
+      right_format = "$time $sudo";
+      format = ''
+      $hostname$directory$git_branch$git_status
+      $character
+      '';
+      directory = {
+        truncation_length = 2;
+        truncation_symbol = "…/";
+      };
+      git_branch = {
+        format = "on [$symbol$branch(:$remote_branch)]($style) ";
+        style = "bold purple";
+      };
+      sudo = {
+        disabled = false;
+        format = "[$symbol]($style)";
+      };
+      time = {
+        disabled = false;
+        format = "[$time]($style)";
+      };
+      env_var.USER = {
+        default = "nn";
+        format = "[$env_value]($style)";
+        style = "201 bold"; # Ansi color 201 https://i.stack.imgur.com/KTSQa.png
+      };
+    };
+  };
+}
