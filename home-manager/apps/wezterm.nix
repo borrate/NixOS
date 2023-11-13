c518923a251f9f533554e754538968b040a278b7
Rodrigo Hume <rodrigo@sopapo.cl>
2023-11-13 09:15:34 -0300

    feat(wezterm): Modifica config
    - Agrega keybindings tipo tmux
    - Agrega transparencia
    - Tabs at bottom

--- home-manager/apps/wezterm.nix
+++ home-manager/apps/wezterm.nix
@@ -18,8 +18,72 @@
 
       -- For example, changing the color scheme:
       config.color_scheme = 'Dracula'
+      config.tab_bar_at_bottom = true
+      config.window_background_opacity = 0.9
+      -- timeout_milliseconds defaults to 1000 and can be omitted
+      config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+      config.keys = {
+        {
+          key = '|',
+          mods = 'LEADER|SHIFT',
+          action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+        },
+        {
+          key = '%',
+          mods = 'LEADER|SHIFT',
+          action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+        },
+        {
+          key = 'n',
+          mods = 'LEADER',
+          action = wezterm.action.ActivateTabRelative(1),
+        },
+        {
+          key = 'p',
+          mods = 'LEADER',
+          action = wezterm.action.ActivateTabRelative(-1),
+        },
+        {
+          key = 'c',
+          mods = 'LEADER',
+          action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+        },
+        {
+          key = 'h',
+          mods = 'LEADER',
+          action = wezterm.action.ActivatePaneDirection 'Left',
+        },
+        {
+          key = 'l',
+          mods = 'LEADER',
+          action = wezterm.action.ActivatePaneDirection 'Right',
+        },
+        {
+          key = 'k',
+          mods = 'LEADER',
+          action = wezterm.action.ActivatePaneDirection 'Up',
+        },
+        {
+          key = 'j',
+          mods = 'LEADER',
+          action = wezterm.action.ActivatePaneDirection 'Down',
+        },
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        {
+          key = 'a',
+          mods = 'LEADER|CTRL',
+          action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+        },
+      }
 
-      -- and finally, return the configuration to wezterm
+      for i = 1, 8 do
+        table.insert(config.keys, {
+          key = tostring(i),
+          mods = 'LEADER',
+          action = wezterm.action.ActivateTab(i - 1),
+        })
+      end
+
       return config
     '';
   };
